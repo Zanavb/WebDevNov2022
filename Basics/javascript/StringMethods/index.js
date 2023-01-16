f89f52972721ee0b12c5f02f1961d7e1229d5209
atiilla <9992685+atiilla@users.noreply.github.com>
2023-01-16 09:20:14 +0100

    string methods

---
+++ Basics/javascript/StringMethods/index.js
@@ -0,0 +1,40 @@
+// List of String Methods
+
+/*
+
+1. charAt()
+2. charCodeAt()
+3. concat()
+4. endsWith()
+5. fromCharCode()
+6. includes()
+7. indexOf()
+8. lastIndexOf()
+9. localeCompare()
+10. match()
+11. repeat()
+12. replace()
+13. search()
+14. slice()
+15. split()
+16. startsWith()
+17. substr()
+18. substring()
+19. toLocaleLowerCase()
+20. toLocaleUpperCase()
+21. toLowerCase()
+22. toString()
+23. toUpperCase()
+24. trim()
+25. valueOf()
+
+*/
+
+var FullText = "Veniam et nulla veniam excepteur aute duis in consequat quis fugiat eu pariatur est enim."
+var result;
+
+// 1. charAt(position<number>) extract char from a string
+
+
+
+console.log(result)
